13e6668459f9e2d7a8ee733320d375ea267b976e
Naomi Dushay <ndushay@stanford.edu>
2015-10-22 18:40:18 -0700

    add method stubs for SolrDocument.to_solr for tests so they pass in the context of the testing app

--- spec/models/spotlight/resources/searchworks_spec.rb
+++ spec/models/spotlight/resources/searchworks_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Spotlight::Resources::Searchworks do
-  
+
   let :exhibit do
     double(solr_data: { }, blacklight_config: Blacklight::Configuration.new)
   end
@@ -24,48 +24,49 @@ describe Spotlight::Resources::Searchworks do
       expect(subject.can_provide?(double(url: "http://searchworks-test.stanford.edu/xyz"))).to eq true
     end
   end
-  
+
   describe "#doc_id" do
     it "should be able to extract DRUIDs from a searchworks url" do
       subject.url = "http://searchworks.stanford.edu/view/xyz"
       expect(subject.doc_id).to eq "xyz"
     end
-    
+
     it "should be able to extract DRUIDs from a searchworks format url" do
       subject.url = "http://searchworks.stanford.edu/view/xf680rd3068.xml"
       expect(subject.doc_id).to eq "xf680rd3068"
     end
   end
-  
+
   describe "#resource" do
     it "should be a Harvestdor::Indexer resource" do
       expect(subject.resource).to be_a_kind_of Harvestdor::Indexer::Resource
     end
-    
+
     it "should have the correct druid" do
       expect(subject.resource.druid).to eq "xf680rd3068"
     end
-    
+
     it "should have the correct indexer" do
       expect(subject.resource.indexer).to eq Spotlight::Dor::Resources.indexer.harvestdor
     end
   end
-  
+
   describe "#to_solr" do
     before do
       allow(Spotlight::Dor::Resources.indexer).to receive(:solr_document)
+      allow_any_instance_of(SolrDocument).to receive(:to_solr).and_return({})
     end
     context "with a collection" do
       before do
         allow(subject.resource).to receive(:collection?).and_return(true)
       end
-      
+
       it "should provide a solr document for the collection" do
         allow(subject.resource).to receive(:items).and_return([])
         expect(Spotlight::Dor::Resources.indexer).to receive(:solr_document).with(subject.resource).and_return({upstream: true})
         expect(subject.to_solr.first).to include :upstream, :spotlight_resource_id_ssim, :spotlight_resource_type_ssim
       end
-      
+
       it "should provide a solr document for the items too" do
         item = double
         allow(subject.resource).to receive(:items).and_return([item])
@@ -76,12 +77,12 @@ describe Spotlight::Resources::Searchworks do
         expect(solr_doc.last).to include :item
       end
     end
-    
+
     context "with a single item" do
       before do
         allow(subject.resource).to receive(:collection?).and_return(false)
       end
-      
+
       it "should provide a solr document for the resource" do
         expect(Spotlight::Dor::Resources.indexer).to receive(:solr_document).with(subject.resource).and_return({upstream: true})
         expect(subject.to_solr.first).to include :upstream, :spotlight_resource_id_ssim, :spotlight_resource_type_ssim
