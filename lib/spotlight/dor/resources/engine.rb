077b7856727b3723e7ab6b26cafc8be0b49df090
Chris Beer <cabeer@stanford.edu>
2015-10-30 13:14:59 -0700

    Update contentMetadata indexer to use IIIF URLs

--- lib/spotlight/dor/resources/engine.rb
+++ lib/spotlight/dor/resources/engine.rb
@@ -8,6 +8,7 @@ module Spotlight::Dor::Resources
       Spotlight::Engine.config.resource_providers << Spotlight::Resources::Searchworks
       Spotlight::Engine.config.resource_providers << Spotlight::Resources::Purl
       Spotlight::Dor::Resources::Engine.config.parallel_options = { in_threads: 1 }
+      Spotlight::Dor::Resources::Engine.config.stacks_iiif_url = 'https://stacks.stanford.edu/image/iiif'
     end
   end
 end
